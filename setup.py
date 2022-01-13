691faa99a2a4c8ba2fe51598dd3f3cb2fe7d058c
piinghel <pjinghelbrecht>
2022-01-13 16:33:28 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="cross_sectional",
     version="0.1.0",
-    packages=find_packages()
+    packages=find_packages("cross_sectional"),
+    install_requires=["pandas>=1.3.4", "plotly>=5.5.0", "numpy>=1.19.2"],
 )
