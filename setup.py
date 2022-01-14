65667e333bea115e5564c5aa19acbd57af3961e6
piinghel <pjinghelbrecht>
2022-01-14 17:53:26 +0100

    update project structure

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from struct import pack
 from setuptools import setup, find_packages
 
 setup(
-    name="cross_sectional",
-    version="0.0.1",
-    packages=find_packages("cross_sectional"),
+    name="cs_trade",
+    version="0.0.2",
+    packages=find_packages(),
+    setup_requires=['wheel'],
     install_requires=["pandas>=1.3.4", "plotly>=5.5.0", "numpy>=1.19.2"],
 )
