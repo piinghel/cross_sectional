bd6048f3729ca6078a55b193c997ddfbdd3018ad
piinghel <pjinghelbrecht>
2022-01-14 21:06:14 +0100

    setup changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cs_trade",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     setup_requires=['wheel'],
     install_requires=["pandas>=1.3.4", "plotly>=5.5.0", "numpy>=1.19.2"],
